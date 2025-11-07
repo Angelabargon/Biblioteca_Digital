145819bbec92fcf20b45a4f0fb3726de5ddbee63
Andres <andres.garciaortega@nervion.salesianas.org>
2025-11-07 19:22:19 +0100

    Creada conexion a la Base de Datos y arreglado error de Vista

--- src/main/java/com/example/biblioteca_digital/conexion/ConexionBD.java
+++ src/main/java/com/example/biblioteca_digital/conexion/ConexionBD.java
@@ -1,4 +1,38 @@
 package com.example.biblioteca_digital.conexion;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexionBD {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/biblioteca_digital";
+    private static final String Usuario = "root";
+    private static final String Contrasena = "";
+
+    private static Connection conexion = null;
+
+    public static Connection getConexion() {
+        try {
+            if (conexion == null || conexion.isClosed()) {
+                conexion = DriverManager.getConnection(URL, Usuario, Contrasena);
+                System.out.println("Conexion establecida");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al conectar con la base de datos");
+        }
+        return conexion;
+    }
+
+    public static void cerrarConexion() {
+        try {
+            if (conexion != null && !conexion.isClosed()) {
+                conexion.close();
+                System.out.println("Conexion cerrada");
+            }
+        } catch (SQLException e) {
+            System.out.println("Eror al cerrar la conexión");
+        }
+    }
+
 }
