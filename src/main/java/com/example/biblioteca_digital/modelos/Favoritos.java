5a4eef6a990384425d2158d4ab56e9e6cbd4f53d
Andres <andres.garciaortega@nervion.salesianas.org>
2025-11-10 12:40:10 +0100

    Terminado el modelo Libros y casi terminado el modelo Favoritos

--- src/main/java/com/example/biblioteca_digital/modelos/Favoritos.java
+++ src/main/java/com/example/biblioteca_digital/modelos/Favoritos.java
@@ -1,6 +1,12 @@
 package com.example.biblioteca_digital.modelos;
 
-public class Favoritos
-{
+public class Favoritos extends Libro {
+
+    int id;
+    Libro libro;
+    int id_libro = getId();
+
+    Usuario usuario;
+    int id_usuario =
 
 }
