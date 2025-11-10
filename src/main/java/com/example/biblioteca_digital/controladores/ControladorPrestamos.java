62e4a64831ee95ee4d7fd4c8239d2de44dbc89c7
AngelaBarGon <angelabarcenaabc@gmail.com>
2025-11-10 12:23:49 +0100

    Commit de Prestamo + enum estado

--- src/main/java/com/example/biblioteca_digital/controladores/ControladorPrestamos.java
+++ src/main/java/com/example/biblioteca_digital/controladores/ControladorPrestamos.java
@@ -1,4 +1,4 @@
 package com.example.biblioteca_digital.controladores;
 
-public class ControladorPrestamos {
+public class ControladorPrestamos{
 }
--- src/main/java/com/example/biblioteca_digital/modelos/Prestamo.java
+++ src/main/java/com/example/biblioteca_digital/modelos/Prestamo.java
@@ -1,6 +1,56 @@
 package com.example.biblioteca_digital.modelos;
 
+import java.util.Date;
+import java.util.EnumMap;
+
 public class Prestamo
 {
+    int id, id_usuario, id_libro;
+    Date fecha_inicio, fecha_fin;
+    Estado estado;
 
+    public Prestamo() {
+    }
+
+    public Prestamo(int id, int id_usuario, int id_libro, Date fecha_inicio, Date fecha_fin, Estado estado) {
+        this.id = id;
+        this.id_usuario = id_usuario;
+        this.id_libro = id_libro;
+        this.fecha_inicio = fecha_inicio;
+        this.fecha_fin = fecha_fin;
+        this.estado = estado;
+    }
+
+    /*Getters de los atributos de una reseña
+
+     */
+    public int getId() {return id;}
+    public int getId_usuario() {return id_usuario;}
+    public int getId_libro() {return id_libro;}
+    public Date getFecha_inicio() {return fecha_inicio;}
+    public Date getFecha_fin() {return fecha_fin;}
+    public Estado getEstado() {return estado;}
+
+    /*Setters de los atributos de una reseña
+
+     */
+    public void setId(int id) {this.id = id;}
+    public void setId_usuario(int id_usuario) {this.id_usuario = id_usuario;}
+    public void setId_libro(int id_libro) {this.id_libro = id_libro;}
+    public void setFecha_inicio(Date fecha_inicio) {this.fecha_inicio = fecha_inicio;}
+    public void setFecha_fin(Date fecha_fin) {this.fecha_fin = fecha_fin;}
+    public void setEstado(Estado estado) {this.estado = estado;}
+
+    @Override
+    public String toString() {
+        return "Prestamo=\n" +
+                "[ID=" + id +
+                "]\n[ID de Usuario=" + id_usuario +
+                "]\n[ID de Libro=" + id_libro +
+                "]\n[Fecha de Inicio=" + fecha_inicio +
+                "]\n[Fecha de Fin=" + fecha_fin +
+                "]\n[Estado=" + estado +
+                "]" +
+                '}';
+    }
 }
