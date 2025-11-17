e2e271b0c278056e4c4467cb6c5b485712d95307
AngelaBarGon <angelabarcenaabc@gmail.com>
2025-11-17 11:55:20 +0100

    Cambios pt2

--- src/main/java/com/example/biblioteca_digital/controladores/ControladorAdministrador.java
+++ src/main/java/com/example/biblioteca_digital/controladores/ControladorAdministrador.java
@@ -5,51 +5,7 @@ import com.example.biblioteca_digital.modelos.Usuario;
 import java.util.List;
 import java.util.Optional;
 
-public class ControladorAdministrador<T, ID> implements Crud<T, ID>
+public class ControladorAdministrador
 {
 
-    public void initializeCuenta(Usuario cuenta) {
-
-    }
-    /**
-     * @param entity
-     * @return
-     */
-    @Override
-    public T guardar(T entity) {
-        return null;
-    }
-
-    /**
-     * @param id
-     * @return
-     */
-    @Override
-    public Optional<T> buscarPorId(ID id) {
-        return Optional.empty();
-    }
-
-    /**
-     * @return
-     */
-    @Override
-    public List<T> listar() {
-        return List.of();
-    }
-
-    /**
-     * @param id
-     */
-    @Override
-    public void eliminarPorId(ID id) {
-
-    }
-
-    /**
-     * @param entity
-     */
-    @Override
-    public void eliminar(T entity) {
-
-    }
 }
--- src/main/java/com/example/biblioteca_digital/controladores/ControladorReseñas.java
+++ src/main/java/com/example/biblioteca_digital/controladores/ControladorReseñas.java
@@ -3,48 +3,7 @@ package com.example.biblioteca_digital.controladores;
 import java.util.List;
 import java.util.Optional;
 
-public class ControladorReseñas<T, ID> implements Crud<T, ID>
+public class ControladorReseñas
 {
 
-    /**
-     * @param entity
-     * @return
-     */
-    @Override
-    public T guardar(T entity) {
-        return null;
-    }
-
-    /**
-     * @param id
-     * @return
-     */
-    @Override
-    public Optional<T> buscarPorId(ID id) {
-        return Optional.empty();
-    }
-
-    /**
-     * @return
-     */
-    @Override
-    public List<T> listar() {
-        return List.of();
-    }
-
-    /**
-     * @param id
-     */
-    @Override
-    public void eliminarPorId(ID id) {
-
-    }
-
-    /**
-     * @param entity
-     */
-    @Override
-    public void eliminar(T entity) {
-
-    }
 }
