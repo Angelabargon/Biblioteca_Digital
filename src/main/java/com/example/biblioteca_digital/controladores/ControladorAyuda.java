c9bd31829587db89f931c6c85f7e7c79760b4a0b
Diego <dalonsorodriguez223@gmail.com>
2025-11-17 13:46:45 +0100

    Vista-Ayuda-Login preparada para refinamiento y ControladorAyuda actualizado

--- src/main/java/com/example/biblioteca_digital/controladores/ControladorAyuda.java
+++ src/main/java/com/example/biblioteca_digital/controladores/ControladorAyuda.java
@@ -1,6 +1,18 @@
 package com.example.biblioteca_digital.controladores;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
 public class ControladorAyuda
 {
+
+    @FXML
+    private void cerrarAyuda(ActionEvent event) {
+        // Cierra la ventana actual
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
 
 }
