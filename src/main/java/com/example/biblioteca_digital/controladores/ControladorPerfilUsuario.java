5ca66bb85aa6c65a4fecc7e2d4684bb65db570f3
Diego <dalonsorodriguez223@gmail.com>
2025-11-25 13:17:33 +0100

    Peueñps avances en el controlador y la vista PerfilUsuario

--- src/main/java/com/example/biblioteca_digital/controladores/ControladorPerfilUsuario.java
+++ src/main/java/com/example/biblioteca_digital/controladores/ControladorPerfilUsuario.java
@@ -1,7 +1,40 @@
 package com.example.biblioteca_digital.controladores;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+
+/*
+Creamos la clase de ControladorPerfilUsuario para mostrar una ventana con los datos del usuario
+junto con las opciones de cambiar contraseña y cerrar sesión.
+ */
 public class ControladorPerfilUsuario {
 
+    @FXML
+    private ImageView iv_icono;
 
+    @FXML
+    private Label lb_nombreUsuario;
 
+    @FXML
+    private Label lb_nombreReal;
+
+    @FXML
+    private Label lb_favoritos;
+
+    @FXML
+    private Label lb_prestamos;
+
+    @FXML
+    private Button bt_cambioContrasena;
+
+    @FXML
+    private Button bt_cerrarSesion;
+
+    /*
+    @FXML
+    public void
+    */
 }
