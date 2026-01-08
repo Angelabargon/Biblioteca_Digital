a9f4c3f0d178a194fa566d474638b7187b4ed197
Diego <dalonsorodriguez223@gmail.com>
2026-01-08 20:05:21 +0100

    Cambios:
    -ControladorLeerLibro: Condicionales mejorados para evitar cuenta en números negativos.
    
    -PrestamoDAO: Partes que cerraban la conexión eliminadas.

--- src/main/java/com/example/biblioteca_digital/DAO/usuario/PrestamoDAO.java
+++ src/main/java/com/example/biblioteca_digital/DAO/usuario/PrestamoDAO.java
@@ -75,7 +75,7 @@ public class PrestamoDAO
                 try
                 {
                     conn.setAutoCommit(true);
-                    conn.close();
+
                 }
                 catch (SQLException closeException)
                 {
@@ -240,7 +240,7 @@ public class PrestamoDAO
                 try
                 {
                     conn.setAutoCommit(true);
-                    conn.close();
+
                 }
                 catch (SQLException closeEx)
                 {
